c8719d9041a91f786e86c8e4889610bf93a84796
Josh McBroom <mcbroomjc@gmail.com>
2016-08-21 15:25:59 -0700

    Wrote initial meal planning function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 var app = angular.module('MagicButton', []);
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log('works!');
+  $scope.allMeals = [];
+  $scope.quantDefault = 5;
+  $scope.myMeals = [];
+
+  $scope.planMeals = function(allMeals, quant) {
+    // ran randomly selects an index from our meals array
+    var ran = Math.floor(Math.random() * allMeals.length);
+    // checking to see if they passed in a quantity of meals
+    if (!quant) {
+      // if there are fewer than 5 meals total in their meals array...
+      if (allMeals.length < 5) {
+        // ... then set their quant to how many meals they have available
+        var quant = allMeals.length + 1;
+        // else...
+      } else {
+        // ... set quant to our default of 5
+        var quant = $scope.quantDefault;
+      }
+    }
+    // looping over our meals while quant is greater than 0
+    while (quant > 0) {
+      // pushing the selected meal to our myMeals array
+      $scope.myMeals.push(allMeals[ran]);
+      // removing the selected meal from our allMeals array, to avoid duplicates
+      allMeals.splice(ran, 1);
+      // decrementing quant, so our while loop won't crash the browser
+      quant -= 1;
+    }
+  };
 }]);
