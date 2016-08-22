e65db72390876f99262e33cc7536df6c9b8d9be9
Josh McBroom <mcbroomjc@gmail.com>
2016-08-22 15:22:43 -0700

    Added Mongoose, setup User Schema

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ var app = express();
 // mongoose dependencies
 var mongoose = require('mongoose');
 // models and mongoose connection can go here
+mongoose.connect('mongodb://localhost/magic-button');
 
 // decode POST data in JSON and URL encoded formats
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('morgan')('dev')); 
+
+app.listen(3000);
---
+++ models/user.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+UserSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    var returnJson = {
+      id: ret._id,
+      email: ret.email,
+      name: ret.name
+    };
+    return returnJson;
+  }
+});
+
+UserSchema.methods.authenticated = function(password, callback) {
+  var user = this;
+  var isAuthenticated = bcrypt.compareSync(password, user.password);
+
+  callback(null, isAuthenticated ? user : false)
+};
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    next();
+  } else {
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
